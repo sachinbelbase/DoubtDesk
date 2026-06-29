70d4e53804e1e8a80592ec8ad32f920d99627572
sachinbelbase <sachinbelbase78@gmail.com>
2026-06-29 19:35:53 +0545

    added day 1 work

---
+++ DoubtDesk/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+        <h1 className="text-3xl font-bold underline text-center py-10">
+          DoubtDesk
+        </h1>
+    </>
+  )
+}
+
+export default App
