e50315ecebed7f27cc1525f011992b3a34ec8d4c
Sabin Dangol <sabindangol33@gmail.com>
2026-08-02 12:15:04 +0545

    gitignore

--- DoubtDesk/backend/database.py
+++ DoubtDesk/backend/database.py
@@ -1,15 +1,19 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:@localhost:3306/doubtdesk"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 session_local = sessionmaker(
     autocommit=False,
-    autoflush=False, 
+    autoflush=False,
     bind=engine
-    )
+)
 
 Base = declarative_base()
 
