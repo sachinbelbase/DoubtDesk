acc880a8352cbffaf1099c51e697e7362a707ad6
sachindada1 <sachinbelbase78@gmail.com>
2026-08-08 17:01:53 +0545

    changes db.py

--- DoubtDesk/Fast API Backend/database.py
+++ DoubtDesk/Fast API Backend/database.py
@@ -1,6 +1,7 @@
 import os
+import ssl
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,13 +14,20 @@ if not DATABASE_URL:
         "DATABASE_URL=mysql+pymysql://user:password@localhost:3306/doubtdesk"
     )
 
-engine = create_engine(DATABASE_URL)
+# Aiven (and most managed MySQL hosts) require SSL. pymysql needs this
+# passed via connect_args rather than a ?ssl-mode=... query string.
+connect_args = {}
+if "aivencloud.com" in DATABASE_URL:
+    ssl_context = ssl.create_default_context()
+    connect_args = {"ssl": ssl_context}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 session_local = sessionmaker(
     autocommit=False,
-    autoflush=False, 
+    autoflush=False,
     bind=engine
-    )
+)
 
 Base = declarative_base()
 
